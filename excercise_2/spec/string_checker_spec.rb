91f8a8b9911823bd2d8773c4f9c989bd41f06026
Marina Papapetrou <contact@marinapapap.com>
2022-11-09 17:07:47 +0000

    string_checker refactor

--- excercise_2/spec/string_checker_spec.rb
+++ excercise_2/spec/string_checker_spec.rb
@@ -6,28 +6,33 @@ require 'string_checker'
 
 RSpec.describe "checks if the string includes '#TODO'" do
 
-  it "returns true if string is equal to '#TODO'" do
-    result = string_checker("#TODO")
-    expect(result).to eq true
+  context "returns true" do
+    it "string is equal to '#TODO'" do
+      result = string_checker("#TODO")
+      expect(result).to eq true
+    end
+
+    it "string includes '#TODO" do
+      result = string_checker("#TODO hello")
+      expect(result).to eq true
+    end
+  end
+ 
+  context "returns false" do
+    it "string doesn't include '#TODO'" do
+      result = string_checker("hello")
+      expect(result).to eq false
+    end
+    
+    it "string is empty" do
+      result = string_checker("")
+      expect(result).to eq false
+    end
   end
 
-  it "returns false if the string doesn't include '#TODO'" do
-    result = string_checker("hello")
-    expect(result).to eq false
+  context "it fails" do
+    it "if nil" do
+      expect { string_checker(nil) }.to raise_error "Invalid nil entry"
+    end
   end
-
-  it "returns true if string includes '#TODO" do
-    result = string_checker("#TODO hello")
-    expect(result).to eq true
-  end
-
-  it "returns false if string is empty" do
-    result = string_checker("")
-    expect(result).to eq false
-  end
-
-  it "it returns error message if nil" do
-    expect { string_checker(nil) }.to raise_error "Invalid nil entry"
-  end
-
 end
