9968bbcfd45a120cb85c7bac279972c975e96a66
Marina Papapetrou <contact@marinapapap.com>
2022-11-09 14:13:31 +0000

    part 1 initial 3 tests

--- excercise_2/lib/reading_time.rb
+++ excercise_2/lib/reading_time.rb
@@ -1,4 +1,4 @@
 def reading_time(text)
-
+  text.split(" ").count / 200
 end
 
--- excercise_2/spec/reading_time_spec.rb
+++ excercise_2/spec/reading_time_spec.rb
@@ -2,6 +2,21 @@ require 'reading_time'
 
 RSpec.describe "give an estimate of someone's reading time in minutes" do
 
-  it "gives "
+  it "returns 0 for an empty string" do
+    result = reading_time("")
+    expect(result).to eq 0
+  end
+
+  it "returns 1 for 200 words" do
+    words = ("hello " * 200).chop
+    result = reading_time(words)
+    expect(result).to eq 1
+  end
+
+  it "returns 2 for 400 words" do
+    words = ("hello " * 400).chop
+    result = reading_time(words)
+    expect(result).to eq 2
+  end
 
 end
